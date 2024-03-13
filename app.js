d1d03d0d887dff7c2ce1e4e2e245a820a615bb21
alfito69420 <panchopistolas159753@gmail.com>
2024-03-13 13:47:15 -0600

    ADD: crud completo

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000
 
 app.use(express.json())
 
+
+
 const CONNECTION_STRING = "mongodb+srv://alfito:xqoyTzOHqCCAcpv8@cluster0.wdpzzmg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 var db = null;
@@ -68,16 +70,47 @@ app.get('/posts', async (req, res) => {
 app.post('/posts', async function (req, res) {
 
     const newPost = {
-        "commenter": req.body.commenter,
-        "comment": req.body.comment,
-        "created_at": req.body.created_at,
-        "post_id": req.body.post_id
+        "title": req.body.title,
+        "author": req.body.author,
+        "content": req.body.content,
+        "image": req.body.image,
+        "category": req.body.category
 
     }
     const result = await db.collection("posts").insertOne(newPost);
 
     res.send(result)
 })
+
+app.put('/posts/update', async function (req, res) {
+
+    try {
+        const empUpdate = db.collection('posts')
+        const result = await empUpdate.findOneAndUpdate(
+            { "post_nmb":parseInt(req.body.post_nmb) },
+            { $set: req.body },
+            { returnDocument: 'after', upsert: true }
+        )
+        res.send(result)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
+//  DELETE ONE POST
+app.delete('/posts/delete/:id', async function (req, res) {
+    try {
+        const result = await db.collection('posts').findOneAndDelete(
+            { "_id": new ObjectId(req.params.id) }
+        )
+        res.send((result) ? "Post deleted..." : "Post not found")
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, async () => {
